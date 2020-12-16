6f1934cc6cc5bf3de1d6a2de3d471eadaafc5778
cheubes <christophe@heubes.com>
2020-12-16 09:58:45 +0100

    Unfollow.

--- instaGH.py
+++ instaGH.py
@@ -19,8 +19,9 @@ with smart_run(session):
     session.set_do_like(enabled=False, percentage=0)
     session.set_do_comment(enabled=False, percentage=0)
     session.set_do_follow(enabled=True, percentage=100, times=1)
+    session.set_dont_include(parameters.dont_include)
 
-    ## Follow likers
+    # Follow likers
     if parameters.do_follow_likers :
         session.follow_likers(
                             parameters.targets,
@@ -30,7 +31,7 @@ with smart_run(session):
                             sleep_delay=600,
                             interact=False)
 
-    ## Follow followers
+    # Follow followers
     if parameters.do_follow_followers :
         session.follow_user_followers(
                             parameters.targets,
@@ -39,7 +40,7 @@ with smart_run(session):
                             sleep_delay=600,
                             interact=False)
 
-    ## Follow following
+    # Follow following
     if parameters.do_follow_following :
         session.follow_user_following(
                             parameters.targets,
@@ -47,3 +48,11 @@ with smart_run(session):
                             randomize=True,
                             sleep_delay=600,
                             interact=False)
+
+    # Unfollow
+    if parameters.do_unfollow :
+        session.unfollow_users(amount=200,
+                            allFollowing=True,
+                            style="FIFO",
+                            unfollow_after=24*60*60,
+                            sleep_delay=600)
