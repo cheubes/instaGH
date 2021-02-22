4d4658a68e217a03aa30c819fb28c2d756629439
cheubes <christophe@heubes.com>
2021-02-22 06:41:03 +0100

    Let's refacto.

--- instaGH.py
+++ instaGH.py
@@ -1,8 +1,11 @@
+import logging
+
 from utils import *
 
 from instapy import InstaPy
 from instapy import smart_run
 
+from rich.logging import RichHandler
 
 parameters = load_parameters()
 
@@ -14,7 +17,9 @@ session = InstaPy(
                 password=parameters.password,
                 headless_browser=parameters.headless_browser,
                 bypass_security_challenge_using='email',
-	            want_check_browser=True)
+	            want_check_browser=True,
+                log_handler=RichHandler(rich_tracebacks=True),
+                show_logs=False)
 
 
 with smart_run(session):
@@ -26,6 +31,13 @@ with smart_run(session):
     session.set_dont_include(parameters.dont_include)
     session.set_skip_users(skip_private=True, skip_no_profile_pic=True, skip_business=True)
 
+    # Unfollow
+    if parameters.do_unfollow :
+        session.unfollow_users(amount=200,
+                            allFollowing=True,
+                            style="FIFO",
+                            unfollow_after=24*60*60,
+                            sleep_delay=sleep_delay)
     # Follow likers
     if parameters.do_follow_likers :
         session.follow_likers(
@@ -36,28 +48,22 @@ with smart_run(session):
                             sleep_delay=sleep_delay,
                             interact=False)
 
-    # Follow followers
-    if parameters.do_follow_followers :
-        session.follow_user_followers(
-                            parameters.targets,
-                            amount=200,
-                            randomize=True,
-                            sleep_delay=sleep_delay,
-                            interact=False)
 
-    # Follow following
-    if parameters.do_follow_following :
-        session.follow_user_following(
-                            parameters.targets,
-                            amount=200,
-                            randomize=True,
-                            sleep_delay=sleep_delay,
-                            interact=False)
 
-    # Unfollow
-    if parameters.do_unfollow :
-        session.unfollow_users(amount=200,
-                            allFollowing=True,
-                            style="FIFO",
-                            unfollow_after=24*60*60,
-                            sleep_delay=sleep_delay)
+    # # Follow followers
+    # if parameters.do_follow_followers :
+    #     session.follow_user_followers(
+    #                         parameters.targets,
+    #                         amount=200,
+    #                         randomize=True,
+    #                         sleep_delay=sleep_delay,
+    #                         interact=False)
+    #
+    # # Follow following
+    # if parameters.do_follow_following :
+    #     session.follow_user_following(
+    #                         parameters.targets,
+    #                         amount=200,
+    #                         randomize=True,
+    #                         sleep_delay=sleep_delay,
+    #                         interact=False)
